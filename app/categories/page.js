49e53d2dab49ed5a77b8f31af4b41676640865a5
Shubhangi555 <bhosaleshubhangi333@gmail.com>
2025-07-21 19:03:36 +0530

    Add files via upload

---
+++ app/categories/page.js
@@ -0,0 +1,31 @@
+import { client } from "/lib/sanity";
+import Link from "next/link";
+import Navbar from "../components/Navbar";
+import "../../styles/categories.css"
+
+export const revalidate = 60;
+
+export default async function CategoriesPage() {
+  const categories = await client.fetch(`
+    *[_type == "category"]{
+      title,
+      "slug": slug.current
+    }
+  `);
+
+  return (
+    <>
+      <Navbar />
+      <main className="category-container">
+        <h1>All Categories</h1>
+        <div className="category-grid">
+          {categories.map((cat) => (
+            <Link href={`/categories/${cat.slug}`} key={cat.slug} className="category-box">
+              {cat.title}
+            </Link>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}

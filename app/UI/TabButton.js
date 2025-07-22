9aa00603d6d0e8ff77adee73b1cdcd201161a6eb
Shubhangi555 <bhosaleshubhangi333@gmail.com>
2025-07-22 23:25:16 +0530

    Add files via upload

---
+++ app/UI/TabButton.js
@@ -0,0 +1,26 @@
+// components/TabButton.js
+
+export default function TabButton({ isSelected, onClick, children }) {
+  return (
+    <button
+    className="tab-btn"
+      onClick={onClick}
+      style={{
+        margin: "5px",
+        borderTop: "none",
+        borderLeft: "none",
+        borderRight: "none",
+        borderBottom: isSelected ? "2px solid grey" : "none",
+        backgroundColor: isSelected ? " transparent" : " transparent",
+        cursor: "pointer",
+        textTransform:"Capitalize",
+        marginBottom:"20px",
+        fontSize:"1rem",
+        padding:"0.3rem"
+       
+      }}
+    >
+      {children}
+    </button>
+  );
+}

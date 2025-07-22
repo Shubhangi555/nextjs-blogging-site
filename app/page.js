12a832cfb9d91b02d7104b0394a94d8380f399c4
Shubhangi555 <bhosaleshubhangi333@gmail.com>
2025-07-22 23:21:53 +0530

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { client } from "/lib/sanity";
 import Navbar from "./components/Navbar";
 import Image from "next/image";
+import CategoryFilterPosts from "./components/CategoryFilterPosts";
+import BannerSlider from "./components/BannerSlider";
 
 export const revalidate = 60;
 
@@ -19,12 +21,25 @@ export default async function HomePage() {
     }
   `);
 
+   const categories = await client.fetch(`
+    *[_type == "category"]{
+      title,
+      "slug": slug.current,
+      "categories": categories[]->title,
+      "mainImage": mainImage.asset->url,
+      "alternativeText": mainImage.alt
+    }
+  `);
+
   return (
     <>
       <Navbar />
+      <BannerSlider/>
+        <CategoryFilterPosts categories={categories} posts={posts}/>
+<section>
       <div className="container">
         <header>
-          <h1>My Blog</h1>
+          <h1>Recent Blogs</h1>
         </header>
         <div className="post-grid">
           {posts.map((post) => {
@@ -37,20 +52,23 @@ export default async function HomePage() {
                   src={post.mainImage}
                   alt={post.alternativeText || post.title || "Blog image"}
                   fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   style={{ objectFit: "cover" }}
                 />
                   </div>
+                  <div className="post-info">
                 <Link href={`/blog/${category}/${post.slug?.current}`} className="post-title">
                   {post.title}
                 </Link>
                 <p className="post-date">{new Date(post.publishedAt).toDateString()}</p>
                 <p className="post-description">{post.excerpt}</p>
                 </div>
-            
+                </div>           
             );
           })}
         </div>
       </div>
+      </section>
     </>
   );
 }

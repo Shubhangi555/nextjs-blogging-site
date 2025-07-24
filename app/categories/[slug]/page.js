75b70403c6cd331f8ea676b30e6bbd18a9f4a7d1
Shubhangi555 <bhosaleshubhangi333@gmail.com>
2025-07-24 13:35:09 +0530

    Add files via upload

--- app/categories/[slug]/page.js
+++ app/categories/[slug]/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { client } from "/lib/sanity";
 import Link from "next/link";
+import { PortableText } from "next-sanity";
 
 export const revalidate = 60;
 // Optional fallback
@@ -16,6 +17,13 @@ export async function generateStaticParams() {
 export default async function CategoryPage({ params }) {
   const { slug } = await params;
 
+  const category = await client.fetch(
+    `*[_type == "category" && slug.current == $slug][0]{
+      title
+    }`,
+    { slug }
+  );
+
   const posts = await client.fetch(
     `*[_type == "post" && references(*[_type == "category" && slug.current == $slug]._id)]{
       _id,
@@ -23,6 +31,7 @@ export default async function CategoryPage({ params }) {
       slug,
       publishedAt,
       excerpt,
+      body,
       "mainImage": mainImage.asset->url,
       "alternativeText": mainImage.alt,
       "category": categories[0]->title
@@ -31,27 +40,38 @@ export default async function CategoryPage({ params }) {
   );
 
   return (
-    <div className="container">
-      <h1>Posts in <q>{slug}</q></h1>
-      {posts.length === 0 && <p>No posts found.</p>}
-      <div className="post-grid">
-        {posts.map((post) => (
-          <div key={post._id} className="post">
-            <div className="image-wrapper" key={post._id}>
-              <Image
-                src={post.mainImage}
-                alt={post.alternativeText || post.title || "Blog image"}
-                fill
-                style={{ objectFit: "cover" }}
-              />
+    <section>
+      <div className="container">
+        <h1>Explore <span>{category.title}</span></h1>
+        {posts.length === 0 && <p>No posts found.</p>}
+        <div className="post-grid">
+          {posts.map((post) => (
+             <Link href={`/blog/${slug}/${post.slug.current}`} key={post._id}>
+            <div  className="post">
+              <div className="image-wrapper" key={post._id}>
+                <Image
+                  src={post.mainImage}
+                  alt={post.alternativeText || post.title || "Blog image"}
+                  fill
+                  style={{ objectFit: "cover" }}
+                />
+              </div>
+             
+                <div className="post-info">
+                  <h3>
+                    {post.title}
+                  </h3>
+
+                  <p className="post-date">{new Date(post.publishedAt).toDateString()}</p>
+                  <div className="post-description">
+                    <PortableText value={post.body} />
+                  </div>
+                </div>
             </div>
-            <Link href={`/blog/${slug}/${post.slug.current}`}>
-              <h3>{post.title}</h3>
             </Link>
-            <p>{post.excerpt}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
